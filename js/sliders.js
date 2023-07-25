a14791f9d75cb58a74da38701670be11ab449fd8
ngf93 <nabiullinaguzel93@gmail.com>
2023-07-25 15:20:01 +0300

    edits

--- js/sliders.js
+++ js/sliders.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper('.swiper-products', {
     loop: true,
     slidesPerView: 4,
+    spaceBetween: 30,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
