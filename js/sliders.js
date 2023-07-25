8ed65da7b900e09c08db7df4260c695a750662e2
ngf93 <nabiullinaguzel93@gmail.com>
2023-07-25 22:45:23 +0300

    quiz desktop

--- js/sliders.js
+++ js/sliders.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-products', {
+const swiper1 = new Swiper('.swiper-4', {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 15,
@@ -22,6 +22,34 @@ const swiper = new Swiper('.swiper-products', {
     1200: {
       slidesPerView: 4,
       spaceBetween: 30,
+    }
+  }
+});
+
+const swiper2 = new Swiper('.swiper-options', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 60,
     }
   }
 });
