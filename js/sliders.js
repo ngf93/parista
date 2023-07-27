83cd1767f63d776b28eb790769545d861c24e054
ngf93 <nabiullinaguzel93@gmail.com>
2023-07-27 19:46:16 +0300

    subcategory desktop

--- js/sliders.js
+++ js/sliders.js
@@ -53,3 +53,31 @@ const swiper2 = new Swiper('.swiper-options', {
     }
   }
 });
+
+const swiper3 = new Swiper('.swiper-3', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
+});
