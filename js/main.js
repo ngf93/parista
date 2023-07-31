2644a50a990df1aa5e94fad2014477b959c19e8f
ngf93 <nabiullinaguzel93@gmail.com>
2023-07-31 19:39:49 +0300

    filter

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ function hideHoverMenu(){
     elem.style.display = "none";
   }
 }
+
+
+
+function handleMinRange(inp){
+  inp.value=Math.min(inp.value, inp.parentNode.childNodes[5].value-1);
+  let value=(100/(parseInt(inp.max)-parseInt(inp.min)))*parseInt(inp.value)-(100/(parseInt(inp.max)-parseInt(inp.min)))*parseInt(inp.min);
+  let children = inp.parentNode.childNodes[1].childNodes;
+  children[1].style.width=value+'%';
+  children[5].style.left=value+'%';
+  children[7].style.left=value+'%';
+  children[11].style.left=value+'%';
+  children[11].childNodes[1].innerHTML=inp.value;
+}
+function handleMaxRange(inp){
+  inp.value=Math.max(inp.value, inp.parentNode.childNodes[3].value-(-1));
+  let value=(100/(parseInt(inp.max)-parseInt(inp.min)))*parseInt(inp.value)-(100/(parseInt(inp.max)-parseInt(inp.min)))*parseInt(inp.min);
+  let children = inp.parentNode.childNodes[1].childNodes;
+  children[3].style.width=(100-value)+'%';
+  children[5].style.right=(100-value)+'%';
+  children[9].style.left=value+'%';
+  children[13].style.left=value+'%';
+  children[13].childNodes[1].innerHTML=inp.value;
+}
